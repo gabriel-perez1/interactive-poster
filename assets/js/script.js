e0646b14b941b02cd68576723876ffd278c1bc37
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-10 21:21:53 -0400

    border and noise constraint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let gap = 10;
 let circNum = 40;
 let circSize = 20;
-let angle = 90;
+let angle = 0;
+let pointNum = 100;
+let rectSize = 800;
 //initial canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -28,6 +30,35 @@ function draw(){
   //for loop: 1. where to start, 2. where to end, 3. increment
   for(let i=0; i < circNum; i++){
     arc(0, 0, circSize + gap * i, circSize + gap * i, angle * i, 360 - angle/2); //x and y position is zero bc it's at the center of translate
+  }
+  pop(); //only used when separating sections with translate
+
+  //title and subtitle
+  push();
+  translate(width/2, height - 150);
+  noStroke();
+  textFont('Times New Roman');
+  textSize(15);
+  textAlign(CENTER, CENTER);
+  text('Love', 0, 0);
+  textSize(12);
+  text('a decision, a judgement, a promise', 0, 20)
+  pop();
+  
+  //border
+  push();
+  translate(width/2, height/2);
+  noFill();
+  stroke('white');
+  strokeWeight(2);
+  rectMode(CENTER);
+  rect(0, 0, rectSize, rectSize);
+
+  //background noise
+  stroke('white');
+  strokeWeight(1);
+  for(let i = 0; i < pointNum; i++); {
+    point(random(-rectSize/2, rectSize/2), random(-rectSize/2, rectSize/2));
   }
   pop();
 }
